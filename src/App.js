f177cf3d5944ebdfd9e8925a4aebb96a4dac69fc
brayden200 <shangyang2005@gmail.com>
2024-08-05 16:08:52 +0800

    modified

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import PostPageHome from './views/PostPageHome';
+import SignUpPage from './views/SignUpPage';
+import LoginPage from './views/LoginPage';
+import { createBrowserRouter,RouterProvider } from 'react-router-dom';
 
 function App() {
+  const router = createBrowserRouter([
+    {path:"/", element:<PostPageHome/>},
+    {path:"/login",element:<LoginPage/>},
+    {path:"/signup",element:<SignUpPage/>}
+  ]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <RouterProvider router={router}/>
   );
 }
 
