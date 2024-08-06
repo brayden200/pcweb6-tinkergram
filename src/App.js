f915c4f0992c7004058f03eea56742d4a8d1d80e
brayden200 <shangyang2005@gmail.com>
2024-08-06 11:22:26 +0800

    final

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import PostPageHome from './views/PostPageHome';
 import SignUpPage from './views/SignUpPage';
 import LoginPage from './views/LoginPage';
 import { createBrowserRouter,RouterProvider } from 'react-router-dom';
+import PostPageAdd from './views/PostPageAdd';
+import PostPageUpdate from './views/PostPageUpdate';
+import PostPageDetails from './views/PostPageDetails';
 
 function App() {
   const router = createBrowserRouter([
     {path:"/", element:<PostPageHome/>},
     {path:"/login",element:<LoginPage/>},
-    {path:"/signup",element:<SignUpPage/>}
+    {path:"/signup",element:<SignUpPage/>},
+    {path:"/add",element:<PostPageAdd/>},
+    {path:"/post/:id",element:<PostPageDetails/>},
+    {path:"/update/:id",element:<PostPageUpdate/>}
   ]);
   return (
   <RouterProvider router={router}/>
